3b167d9c4ee3305e4603456cadd388c45db3afa6
Justin Lidard <jlidard@umd.edu>
2023-11-21 23:27:21 -0500

    Run offline

--- scripts/run_slum_job.sh
+++ scripts/run_slum_job.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=28        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
 #SBATCH --time=00:60:00         # total run time limit (HH:MM:SS)
-#SBATCH --output=../logs/hallway.out
+#SBATCH --output=../logs/hallway-%Y_%m_%d_%I_%M_%p.out
 ##SBATCH --mail-type=begin        # send email when job begins
 ##SBATCH --mail-type=end          # send email when job ends
 ##SBATCH --mail-user=<YourNetID>@princeton.edu
