d0c258cea304e2e7392292ddb0e5857392fa2126
Justin Lidard <jlidard@umd.edu>
2023-11-21 23:25:16 -0500

    Run offline

--- scripts/run_slum_job.sh
+++ scripts/run_slum_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=28        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
-#SBATCH --time=00:01:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=00:60:00         # total run time limit (HH:MM:SS)
 #SBATCH --output=../logs/hallway.out
 ##SBATCH --mail-type=begin        # send email when job begins
 ##SBATCH --mail-type=end          # send email when job ends
