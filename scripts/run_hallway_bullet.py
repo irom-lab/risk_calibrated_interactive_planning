0e22984d07a70bb47ef8822ec11ff051d9f6c519
Justin Lidard <jlidard@princeton.edu>
2023-12-08 12:43:36 -0500

    Add back randomness

--- scripts/run_hallway_bullet.py
+++ scripts/run_hallway_bullet.py
@@ -131,8 +131,8 @@ if __name__ == ("__main__"):
             if ep_mean_reward >= best_mean_reward:
                 model.save(os.path.join(models_dir, f"model_best_{iter}"))
 
-        if iter % 1 == 0:
-            record_video(videnv, model, video_length=video_length, num_videos=5)
+        if iter % 100 == 0:
+            record_video(videnv, model, video_length=video_length, num_videos=2)
 
 
         wandb.log(training_dict)
